69de532cfeb7a770d7e77e330a510c4054241cfe
Ross Perkel <rossperkel@gmail.com>
2019-09-09 18:30:30 -0400

    working version preMonday Dnd

--- src/index.js
+++ src/index.js
@@ -3,6 +3,9 @@ const ARTISTS_URL = `${BASE_URL}/artists`
 const ARTWORKS_URL = `${BASE_URL}/artworks`
 
 const main = document.querySelector("main")
+const form = document.querySelector("form")
+const createButton = document.querySelector("#create")
+
 
 
 
@@ -12,6 +15,7 @@ function getArtworks() {
     fetch(`${ARTWORKS_URL}`).then(res => res.json())
     .then((artworkObjArr) => {
         main.innerHTML = ""
+        main.innerHTML += ""
         artworkObjArr.data.forEach(addArtworkToDOM)
     }) //end of fetch
 } // end of getArtworks
@@ -31,22 +35,31 @@ function addArtworkToDOM(artworkObj){
 }
 
 function addArtworkToDB(event) {
+    let artistID = parseInt(event.target.artist.value)
+    let newArtTitle = event.target.artworkTitle.value
+    let newArtMedium = event.target.medium.value
+    let newArtSource = event.target.source.value
     console.log(event)
-    console.log(event.target.dataset)
-    debugger
-    fetch(ARTWORKS_URL, {
+    console.log(event.target)
+    // debugger
+    fetch(`${ARTWORKS_URL}`, {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json',
             'Accept': 'application/json'
         },
         body: JSON.stringify({
-            artist_id: artistID
+            artist_id: artistID,
+            title: newArtTitle,
+            medium: newArtMedium,
+            source: newArtSource,
         })
     })
         .then(res => res.json())
         .then(obj => {
+            form.classList = 'hidden'
             console.log(obj);
+            // debugger
             getArtworks();
         })
 }
@@ -58,31 +71,37 @@ function deleteArtwork(event){
         method: "DELETE",
         headers: {"Content-Type": "application/json", "Accept": "application/json"}
     }
-    console.log(event.target.attributes)
-    debugger
     fetch(`${ARTWORKS_URL}/${artworkId}`, config)
-    .then(response => response.json())
-    .then(obj => getArtworks()) //end of fetch
+    .then((  ) => getArtworks()) //end of fetch
 }; //end of deleteArtwork
 
 function buttonHandler(event) {
     if (event.target.classList.contains("delete")) {
         console.log("A delete button was clicked")
         deleteArtwork(event);
-    }
+    } //doesn't auto update yet??
     else if (event.target.classList.contains("edit")) {
         console.log("an edit button was clicked")
         // editArtwork(event);
-    }
-    else if (event.target.classList.contains("create")) {
-        console.log("a create button was clicked")
-        addArtworkToDB(event);
     }
 }//end of button handler
 
 document.addEventListener("DOMContentLoaded", (e) => {
     getArtworks()
-})//end of onLoad getArtworks
+})//end of onLoad getArtworks()
 
 main.addEventListener('click', buttonHandler);
-//listens for button clicks
+//listens for button clicks on artwork divs
+
+createButton.addEventListener('click', (event) => {
+    form.classList = "visible"
+    console.log("a create button was clicked")
+}) //reveals the form when Create button is clicked
+
+form.addEventListener('submit', (event) =>{
+    event.preventDefault()
+    console.log("a submit button was clicked")
+    addArtworkToDB(event);
+    form.reset()
+    form.classList = "hidden"
+})
